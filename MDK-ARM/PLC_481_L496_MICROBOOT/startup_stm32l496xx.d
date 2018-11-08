c86b52533d70926d152821740190d65286af0f52
rms <rmnsfx@yandex.ru>
2018-11-08 09:38:54 +0500

    init

---
+++ MDK-ARM/PLC_481_L496_MICROBOOT/startup_stm32l496xx.d
@@ -0,0 +1,1 @@
+plc_481_l496_microboot\startup_stm32l496xx.o: startup_stm32l496xx.s
---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'PLC_481_L496_MICROBOOT' 
+ * Target:  'PLC_481_L496_MICROBOOT' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
